9f0b74053de6d1590b5c99a06caccbc605514b66
namasayafrans <sinagafrans38@gmail.com>
2022-11-07 00:41:24 +0700

    folderbaru

---
+++ Modulpertama/latihan.py
@@ -0,0 +1,8 @@
+a = input("masukan nilai ke-1 : ")
+b = input("masukan nilai ke-2 : ")
+if a > b:
+    print(a)
+    print("nilai 1 lebih besar daripada nilai 2")
+elif a < b:
+    print(b)
+    print("nilai 2 lebih besar daripada nilai 1")
