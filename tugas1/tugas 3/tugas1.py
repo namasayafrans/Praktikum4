4a91d3aff991009021951c418f023237ee328849
namasayafrans <sinagafrans38@gmail.com>
2022-11-07 00:27:17 +0700

    karna ada kesalahan tekknis

--- tugas1/tugas 3/tugas1.py
+++
@@ -1,8 +0,0 @@
-a = input("masukan nilai a : ")
-b = input("masukan nilai b : ")
-if a > b:
-    print(a)
-    print( "nilai a lebih besar dari nilai b ")
-elif a < b:
-    print(b)
-    print("nilai b lebih besar dari nilai a")
