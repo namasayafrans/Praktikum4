9574c38e2f3f26ea1e29d0665b2bae7ff3f35f67
namasayafrans <sinagafrans38@gmail.com>
2022-11-07 00:20:37 +0700

    tugas

---
+++ tugas1/tugas 3/tugas1.py
@@ -0,0 +1,8 @@
+a = input("masukan nilai a : ")
+b = input("masukan nilai b : ")
+if a > b:
+    print(a)
+    print( "nilai a lebih besar dari nilai b ")
+elif a < b:
+    print(b)
+    print("nilai b lebih besar dari nilai a")
